473a58519be9482e36fece1ddb8e8becad3ce73d
Denise Estipona <unstableintrovert420@gmail.com>
2026-04-16 02:10:31 -0500

    added starting code for game

---
+++ src/project.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+def main():
+    pygame.init()
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
